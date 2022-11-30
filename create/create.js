9c8b4462b29eb206b8880ffbcd8497eead4bd915
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-11-30 11:51:06 -0800

    worked on create.js. Going to check for bugs now. My site was having a lot of issues earlier so I'm hoping most, if not all, were resolved.

--- create/create.js
+++ create/create.js
@@ -5,22 +5,39 @@ const logoutButton = document.getElementById('logout');
 
 form.addEventListener('submit', async (e) => {
     // prevent default
-
+    e.preventDefault();
     // get the name and family id from the form
+    const formData = new FormData(form);
+    const familyId = formData.get('family');
+    const name = formData.get('bunny-name');
 
     // use createBunny to create a bunny with this name and family id
-
+    await createBunny({
+        name: name,
+        family_id: familyId,
+    });
     form.reset();
 });
 
 window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
+    const select = document.querySelector('select');
     // go get the families from supabase
+    const families = await getFamilies();
     // for each family
     // create an option tag
-    // set the option's value and text content
-    // and append the option to the select
+
+    for (let family of families) {
+        const option = document.createElement('option');
+
+        // set the option's value and text content
+        option.value = family.id;
+        option.textContent = family.name;
+
+        // and append the option to the select
+        select.append(option);
+    }
 });
 
 checkAuth();
