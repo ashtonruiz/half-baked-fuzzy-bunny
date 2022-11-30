85c8a623bee5938fc4b24993140ee54872cc5395
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-11-30 11:11:12 -0800

    added supabase URL and KEY, worked on getFamilies and deleteBunny for no particular reason other than they were on the fetch-utils page lol

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,7 @@
 // Create your own supabase database using the provided seeds.sql file
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://nwxkvnsiwauieanvbiri.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwMzQzNCwiZXhwIjoxOTUyNTc5NDM0fQ.8XIsU0FANdaNeQnT-DojpTL-GTlTPZ4CYZDEetpFpWc';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -10,12 +11,19 @@ export function getUser() {
 
 export async function getFamilies() {
     // fetch all families and their bunnies
+    const response = await client.from('loving_families').select();
     // return checkError(response);
+    return checkError(response);
 }
 
 export async function deleteBunny(id) {
     // delete a single bunny using the id argument
+    const response = await client
+        .from('fuzzy_bunnies')
+        .delete()
+        .match({ user_id: getFamilies().id });
     // return checkError(response);
+    return checkError(response);
 }
 
 export async function createBunny(bunny) {
